734703f1543290fb235e47868cf6fc6688d4cafa
SoumavoBhadury <taniyabhaduri93@gmail.com>
2023-09-23 13:19:38 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import streamlit as st
 from ultralytics import YOLO
+import cv2
 from ultralytics.yolo.v8.detect.predict import DetectionPredictor
-
+import time
+from playsound import playsound
+from camera_input_live import camera_input_live
 
 model = YOLO('../Models/final.pt')
+# src = 'download.jpg'
+ptime = time.process_time_ns()
+
+
 def predict():
-    results = model.predict(source="0", show=True, conf=0.5)
-    print(results)
-    #print(type(results))
-
-
-
+    global probs
+    results = model.predict(source="0", show=True)
+    for result in results:
+        probs = result.probs
+        if (float(probs.data[1]) >= 0.50) and (ptime >= 2000000000):
+            st.text('Slippery surface detected')
+            # playsound('/Models/beep-01a (1).wav')
 
 
 def main():
-
-    #title
+    # title
     st.title('See Ya Sense')
-    #button
-    st.button('Check ', on_click=predict())
-    predict()
+    # button
+    st.button('Check', on_click=predict, type='primary')
+
 
 if __name__ == '__main__':
     main()
